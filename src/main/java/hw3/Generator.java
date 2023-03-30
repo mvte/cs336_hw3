3ffa42b49e8e27cf4913f4cfa6584a45151f1bca
jan <57322982+mvte@users.noreply.github.com>
2023-03-29 22:45:26 -0400

    start building generator

--- src/main/java/hw3/Generator.java
+++ src/main/java/hw3/Generator.java
@@ -1,22 +1,112 @@
 package hw3;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.*;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class Generator {
-    Connection con;
+    Connection dbcon;
 
     public Generator(String db, String user, String pass) throws Exception {
         Class.forName("com.mysql.jdbc.Driver");
-        con = DriverManager.getConnection(db, user, pass);
+        dbcon = DriverManager.getConnection(db, user, pass);
+    }
+
+    /**
+     * test constructor
+     */
+    private Generator() {
+    }
+
+    /**
+     * Generates and inserts into database 100 names.
+     * @param reset true if we want to reset the values already in the table
+     */
+    public void generateNames(boolean reset) {
+        final String endpoint = "https://randommer.io/api/Name";
+        final String query = "?nameType=fullname&quantity=104";
+        final String api_key = System.getenv("key");
+
+        JSONArray raw = retrieve(endpoint, query, api_key);
+        if(raw == null) {
+            System.out.println("something went wrong retrieving names");
+            return;
+        }
+
+        try {
+            insert(raw, reset);
+        } catch (Exception e) {
+            System.out.println("something went wrong inserting values into db");
+        }
+
 
     }
 
+    private JSONArray retrieve(String endpoint, String query, String api_key) {
+        try {
+            URL url = new URL(endpoint + query);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("X-Api-Key", api_key);
+
+            int statusCode = connection.getResponseCode();
+            if (statusCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                JSONParser parse = new JSONParser();
+                return (JSONArray) parse.parse(String.valueOf(response));
+            } else {
+                System.out.println("status code: " + statusCode);
+                return null;
+            }
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private void insert(JSONArray arr, boolean reset) throws SQLException {
+        String s = "insert into students values(?, ?, ?)";
+        PreparedStatement p = dbcon.prepareStatement(s);
+        for(Object obj : arr) {
+            String full_name = obj.toString();
+            String hash = String.valueOf(Math.abs(full_name.hashCode()));
+            int id;
+            if(hash.length() < 9) {
+                while(hash.length() != 9) hash += "0";
+                id = Integer.parseInt(hash);
+            } else {
+                id = Integer.parseInt(hash.substring(0,9));
+            }
+            String[] fl = full_name.split(" ");
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Generator gen = new Generator();
+        gen.generateNames(false);
+    }
 }
 
 /*
 students
     use an api to generate 100 names
     for their id, truncate the hashcode of their first name last name to 9 digits
-        attempt insert, if fail, increment their hashcode by 1
+        attempt insert. if fail, increment their hashcode by 1
 
 classes
     should have 5*8 = 40ish classes
@@ -44,6 +134,4 @@ has taken
     iterate 104/13 times
         each iteration, trim random 13 students
         add 4-5 classes (4 or 5 randomly generated)
-
-
  */
--- src/main/java/hw3/Main.java
+++ src/main/java/hw3/Main.java
@@ -3,12 +3,16 @@ package hw3;
 public class Main {
     static final String db = "";
     static final String user = "root";
-    static final String pass = "micaela";
+    static final String pass = System.getenv("pass");
+
     public static void main(String[] args) {
+        Generator generator = null;
         try {
-            Generator generator = new Generator(db, user, pass);
+            generator = new Generator(db, user, pass);
         } catch (Exception e) {
             System.out.println("Error connecting to database");
         }
+
+        generator.generateNames(false);
     }
 }
